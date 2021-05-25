37a3be71b2470acc6514767e84e489bff7459cf4
Gniloe-Aloe <garaino@mail.ru>
2021-05-25 16:14:40 +0300

    Сглаживание, текстуры, спрайты и их изменение

--- main.cpp
+++ main.cpp
@@ -4,8 +4,12 @@
 
 int main()
 {
+	// Устанавливаем 8-й уровень сглаживания
+	sf::ContextSettings settings;
+	settings.antialiasingLevel = 8;
+
 	// Объект, который, собственно, является главным окном приложения
-	sf::RenderWindow window(sf::VideoMode(800, 400), "Tetris");
+	sf::RenderWindow window(sf::VideoMode(800, 400), "Tetris", sf::Style::Default, settings);
 
 	// Главный цикл приложения. Выполняется, пока открыто окно
 	while (window.isOpen())
@@ -51,14 +55,41 @@ int main()
 		//задаём вершины многоугольника
 		sf::ConvexShape figure;
 		figure.setPointCount(4);
-		figure.setPoint(0, sf::Vector2f(500, 15));
-		figure.setPoint(1, sf::Vector2f(600, 15));
-		figure.setPoint(2, sf::Vector2f(600, 150));
-		figure.setPoint(3, sf::Vector2f(500, 150));
-		figure.setFillColor(sf::Color::Green);
-		figure.move(120, 0);
+		figure.setPoint(0, sf::Vector2f(0, 0));
+		figure.setPoint(1, sf::Vector2f(100, 30));
+		figure.setPoint(2, sf::Vector2f(100, 130));
+		figure.setPoint(3, sf::Vector2f(0, 100));
+		figure.setFillColor(sf::Color::Black);
+		figure.move(600, 15);
 		window.draw(figure);
 
+		//прямоугольник
+		sf::RectangleShape rectangle (sf::Vector2f(200, 150));
+		rectangle.setFillColor(sf::Color::Magenta);
+		rectangle.move(30, 150);
+		window.draw(rectangle);
+
+		//текстуры
+		sf::Texture brick_texture;
+		//тут нужно указать своё расположение картинки и не забыть про два слэша
+		//второй аргумент для загрузки части изображения, начиная с координат (0;0)
+		brick_texture.loadFromFile("C:\\Users\\Gniloe_Aloe\\Desktop\\Tetris\\brick.jpg", sf::IntRect(0, 0, 1000, 1000));
+		sf::Sprite brick_sprite(brick_texture, sf::IntRect(0, 0, 200, 200));
+		brick_texture.setSmooth(true);
+		brick_sprite.move(300, 150);
+		window.draw(brick_sprite);
+
+		//синий спрайт
+		sf::Sprite blue_brick(brick_texture, sf::IntRect(0, 0, 200, 200));
+		blue_brick.setColor(sf::Color::Blue);
+		blue_brick.move(520, 150);
+
+		blue_brick.move(100, 0);
+		blue_brick.setRotation(45.0f);
+		blue_brick.setScale(0.5, 0.5);
+		window.draw(blue_brick);
+		
+		
 
 		// Отрисовка окна	
 		window.display();
