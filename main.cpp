da65061ada69495349c6a2d20d5cc3359997cdc9
Gniloe-Aloe <garaino@mail.ru>
2021-06-28 01:13:55 +0300

    Изменена кодировка на UTF-8

--- main.cpp
+++ main.cpp
@@ -1,26 +1,26 @@
-#include <SFML/Graphics.hpp>
+п»ї#include <SFML/Graphics.hpp>
 #include <ctime>
 #include <vector>
 #include <string>
 //C:\\Users\\Gniloe_Aloe\\Desktop\\Tetris\\block.png
 
-//глобальные константы для игрового поля
-const unsigned int CAGE_SIZE = 45;// длинна стороны блока в пикселях
-const unsigned int FIELD_WIDTH = 10;// ширина поля в блока
-const unsigned int FIELD_HEIGHT = 20;// высота поля в блоках
-sf::RenderWindow gaming_window(sf::VideoMode(CAGE_SIZE* FIELD_WIDTH, CAGE_SIZE* FIELD_HEIGHT), "Tetris!"); // Окно игры
-sf::RenderWindow setting_window(sf::VideoMode(CAGE_SIZE* FIELD_WIDTH, CAGE_SIZE* FIELD_HEIGHT), "Tetris!"); // Окно настроек
-//шрифт
+//РіР»РѕР±Р°Р»СЊРЅС‹Рµ РєРѕРЅСЃС‚Р°РЅС‚С‹ РґР»СЏ РёРіСЂРѕРІРѕРіРѕ РїРѕР»СЏ
+const unsigned int CAGE_SIZE = 45;// РґР»РёРЅРЅР° СЃС‚РѕСЂРѕРЅС‹ Р±Р»РѕРєР° РІ РїРёРєСЃРµР»СЏС…
+const unsigned int FIELD_WIDTH = 10;// С€РёСЂРёРЅР° РїРѕР»СЏ РІ Р±Р»РѕРєР°
+const unsigned int FIELD_HEIGHT = 20;// РІС‹СЃРѕС‚Р° РїРѕР»СЏ РІ Р±Р»РѕРєР°С…
+sf::RenderWindow gaming_window(sf::VideoMode(CAGE_SIZE* FIELD_WIDTH, CAGE_SIZE* FIELD_HEIGHT), "Tetris!"); // РћРєРЅРѕ РёРіСЂС‹
+sf::RenderWindow setting_window(sf::VideoMode(CAGE_SIZE* FIELD_WIDTH, CAGE_SIZE* FIELD_HEIGHT), "Tetris!"); // РћРєРЅРѕ РЅР°СЃС‚СЂРѕРµРє
+//С€СЂРёС„С‚
 sf::Font Arial;
-//очки
+//РѕС‡РєРё
 unsigned int score;
 
 
 class Block {
 public:
-	//отображать ли блок
+	//РѕС‚РѕР±СЂР°Р¶Р°С‚СЊ Р»Рё Р±Р»РѕРє
 	bool available = false;
-	//установлен ли блок на поле (не игроок)
+	//СѓСЃС‚Р°РЅРѕРІР»РµРЅ Р»Рё Р±Р»РѕРє РЅР° РїРѕР»Рµ (РЅРµ РёРіСЂРѕРѕРє)
 	bool set_on_field = false;
 	sf::Sprite block_sprite;
 	int x;
@@ -40,7 +40,7 @@ public:
 	void set_position(const int& SIZE) {
 		block_sprite.move(x * SIZE, y * SIZE);
 	}
-	//rand()%5 для перебора всех цветов
+	//rand()%5 РґР»СЏ РїРµСЂРµР±РѕСЂР° РІСЃРµС… С†РІРµС‚РѕРІ
 	void set_color(int color_index) {
 		switch (color_index) {
 		case 0:
@@ -62,28 +62,28 @@ public:
 	}
 
 };
-//создание глобального двумерного массива из блоков под игровое поле
+//СЃРѕР·РґР°РЅРёРµ РіР»РѕР±Р°Р»СЊРЅРѕРіРѕ РґРІСѓРјРµСЂРЅРѕРіРѕ РјР°СЃСЃРёРІР° РёР· Р±Р»РѕРєРѕРІ РїРѕРґ РёРіСЂРѕРІРѕРµ РїРѕР»Рµ
 Block field[FIELD_WIDTH][FIELD_HEIGHT];
 
 class Player {
 public:
 	int x = FIELD_WIDTH/2;
 	int y = 0;
-	//провека возможности движения вправо если не вышли за пределы поля
-	//и справа нет стоящего блока
+	//РїСЂРѕРІРµРєР° РІРѕР·РјРѕР¶РЅРѕСЃС‚Рё РґРІРёР¶РµРЅРёСЏ РІРїСЂР°РІРѕ РµСЃР»Рё РЅРµ РІС‹С€Р»Рё Р·Р° РїСЂРµРґРµР»С‹ РїРѕР»СЏ
+	//Рё СЃРїСЂР°РІР° РЅРµС‚ СЃС‚РѕСЏС‰РµРіРѕ Р±Р»РѕРєР°
 	bool right_resolution(){
 		return (this->x < FIELD_WIDTH - 1)&&(!field[this->x+1][y].available);
 	}
-	//проверка возможности движения влево
+	//РїСЂРѕРІРµСЂРєР° РІРѕР·РјРѕР¶РЅРѕСЃС‚Рё РґРІРёР¶РµРЅРёСЏ РІР»РµРІРѕ
 	bool left_resolution() {
 		return (this->x > 0)&&(!field[x-1][y].available);
 	}
-	//проверка наличия свободного места под фигурой игрока
+	//РїСЂРѕРІРµСЂРєР° РЅР°Р»РёС‡РёСЏ СЃРІРѕР±РѕРґРЅРѕРіРѕ РјРµСЃС‚Р° РїРѕРґ С„РёРіСѓСЂРѕР№ РёРіСЂРѕРєР°
 	bool down_resolution() {
-		//если не вышли за ганицы поля и под нами нет другого стоящего блока
+		//РµСЃР»Рё РЅРµ РІС‹С€Р»Рё Р·Р° РіР°РЅРёС†С‹ РїРѕР»СЏ Рё РїРѕРґ РЅР°РјРё РЅРµС‚ РґСЂСѓРіРѕРіРѕ СЃС‚РѕСЏС‰РµРіРѕ Р±Р»РѕРєР°
 		return (y < FIELD_HEIGHT - 1) && (!field[x][y + 1].available);
 	}
-	//установка блока игрока на поле
+	//СѓСЃС‚Р°РЅРѕРІРєР° Р±Р»РѕРєР° РёРіСЂРѕРєР° РЅР° РїРѕР»Рµ
 	void set_on_field(){
 		field[this->x][this->y].set_on_field = true;
 		//field[this->x][this->y].available = true;
@@ -96,10 +96,10 @@ public:
 
 	}
 
-	// Конец игры
+	// РљРѕРЅРµС† РёРіСЂС‹
 	void game_over()
 	{
-		//Подключение шрифта и работа с текстом
+		//РџРѕРґРєР»СЋС‡РµРЅРёРµ С€СЂРёС„С‚Р° Рё СЂР°Р±РѕС‚Р° СЃ С‚РµРєСЃС‚РѕРј
 		
 
 		sf::Text endgame_text;
@@ -141,7 +141,7 @@ public:
 		}
 	}
 
-	//падение
+	//РїР°РґРµРЅРёРµ
 	void fall() {
 		if (down_resolution()) {
 			++y;
@@ -150,7 +150,7 @@ public:
 	}
 };
 
-// Проверяем линию на заполненность
+// РџСЂРѕРІРµСЂСЏРµРј Р»РёРЅРёСЋ РЅР° Р·Р°РїРѕР»РЅРµРЅРЅРѕСЃС‚СЊ
 bool check_line(const int& y){
 	for (int i = 0; i < FIELD_WIDTH; ++i){
 		if (!field[i][y].available){
@@ -160,7 +160,7 @@ bool check_line(const int& y){
 	return true;
 }
 
-// Удаляем линию
+// РЈРґР°Р»СЏРµРј Р»РёРЅРёСЋ
 void delete_line(const int& y){
 	for (int i = 0; i < FIELD_WIDTH; ++i){
 		field[i][y].available = false;
@@ -169,9 +169,9 @@ void delete_line(const int& y){
 	score++;
 }
 
-// Сдвиг линий вниз
+// РЎРґРІРёРі Р»РёРЅРёР№ РІРЅРёР·
 void fall_line(const int& y){
-	// Сдвигаем изображения, опираясь на set_on_field
+	// РЎРґРІРёРіР°РµРј РёР·РѕР±СЂР°Р¶РµРЅРёСЏ, РѕРїРёСЂР°СЏСЃСЊ РЅР° set_on_field
 	for (int i = y - 1; i > 0; --i){
 		for (int j = 0; j < FIELD_WIDTH; ++j){
 
@@ -187,7 +187,7 @@ void fall_line(const int& y){
 	}
 }
 
-//для настроек
+//РґР»СЏ РЅР°СЃС‚СЂРѕРµРє
 void demonsration_setting_block(sf::Sprite* set_blocks_sprites, int size, sf::Texture& set_block) {
 	for (int i = 0; i < 5; ++i) {
 		set_blocks_sprites[i].setPosition(75 + (5 * 30), 15 + ((19 - i) * 30));
@@ -216,41 +216,41 @@ void demonsration_setting_block(sf::Sprite* set_blocks_sprites, int size, sf::Te
 
 int main() {
 
-	//подключение файлов и инициализация констант
+	//РїРѕРґРєР»СЋС‡РµРЅРёРµ С„Р°Р№Р»РѕРІ Рё РёРЅРёС†РёР°Р»РёР·Р°С†РёСЏ РєРѕРЅСЃС‚Р°РЅС‚
 	sf::Texture BLOCK_TEXTURE;
-	//диапозон блоков 3 - 5
+	//РґРёР°РїРѕР·РѕРЅ Р±Р»РѕРєРѕРІ 3 - 5
 
 	BLOCK_TEXTURE.loadFromFile("pic//block5_o.png");
 	
 
 	sf::Sprite block_sprite(BLOCK_TEXTURE);
 
-	//цвет фона
+	//С†РІРµС‚ С„РѕРЅР°
 	const sf::Color background(130, 155, 207);
 
-	//текстура заднего фона
+	//С‚РµРєСЃС‚СѓСЂР° Р·Р°РґРЅРµРіРѕ С„РѕРЅР°
 	sf::Texture BACKGROUND_TEXTURE;
-	//диапозон фонов 1-4
+	//РґРёР°РїРѕР·РѕРЅ С„РѕРЅРѕРІ 1-4
 	
 	BACKGROUND_TEXTURE.loadFromFile("pic//background3.png");
 	sf::Sprite background_sprite(BACKGROUND_TEXTURE);
 
-	//текстура игровой сетки
+	//С‚РµРєСЃС‚СѓСЂР° РёРіСЂРѕРІРѕР№ СЃРµС‚РєРё
 	sf::Texture CAGE_TEXTURE;
 	
 	CAGE_TEXTURE.loadFromFile("pic//gaming_cage.png");
 	sf::Sprite gaming_cage(CAGE_TEXTURE);
 
-	//Подключение шрифта и работа с текстом
+	//РџРѕРґРєР»СЋС‡РµРЅРёРµ С€СЂРёС„С‚Р° Рё СЂР°Р±РѕС‚Р° СЃ С‚РµРєСЃС‚РѕРј
 	Arial.loadFromFile("arial.ttf");
 
 	
 	srand(time(NULL));
 	
-	//настраиваем игровое поле
+	//РЅР°СЃС‚СЂР°РёРІР°РµРј РёРіСЂРѕРІРѕРµ РїРѕР»Рµ
 	for (int i = 0; i < FIELD_WIDTH; ++i) {
 		for (int j = 0; j < FIELD_HEIGHT; ++j) {
-			//задаём свойства для каждого блока
+			//Р·Р°РґР°С‘Рј СЃРІРѕР№СЃС‚РІР° РґР»СЏ РєР°Р¶РґРѕРіРѕ Р±Р»РѕРєР°
 			field[i][j].available = false;
 			field[i][j].set_texture(BLOCK_TEXTURE);
 			field[i][j].set_coordinate(i, j);
@@ -259,35 +259,35 @@ int main() {
 		}
 	}
 
-	//создаём игрока
+	//СЃРѕР·РґР°С‘Рј РёРіСЂРѕРєР°
 	Player player;
 
 
 
 
-	//создаём окно игры
+	//СЃРѕР·РґР°С‘Рј РѕРєРЅРѕ РёРіСЂС‹
 	
 	gaming_window.clear(background);
 
-	//количество убранных линий
+	//РєРѕР»РёС‡РµСЃС‚РІРѕ СѓР±СЂР°РЅРЅС‹С… Р»РёРЅРёР№
 	score = 0;
 
-	//создание таймера и переменной задержки для нажатия на клавиши
+	//СЃРѕР·РґР°РЅРёРµ С‚Р°Р№РјРµСЂР° Рё РїРµСЂРµРјРµРЅРЅРѕР№ Р·Р°РґРµСЂР¶РєРё РґР»СЏ РЅР°Р¶Р°С‚РёСЏ РЅР° РєР»Р°РІРёС€Рё
 	sf::Clock pressing_timer;
 	sf::Time pressing_delay = sf::seconds(0.01);
 
-	//таймер падения
+	//С‚Р°Р№РјРµСЂ РїР°РґРµРЅРёСЏ
 	sf::Clock falling_timer;
 	const sf::Time difficulty_level = sf::seconds(0.5);
 	sf::Time falling_delay = difficulty_level;
 
-	//флаги нажатия кнопок
+	//С„Р»Р°РіРё РЅР°Р¶Р°С‚РёСЏ РєРЅРѕРїРѕРє
 	bool left_was_pressed = false;
 	bool right_was_pressed = false;
 	bool up = false;
 	bool down_was_pressed = false;
 
-	//текстуры для окна настроек
+	//С‚РµРєСЃС‚СѓСЂС‹ РґР»СЏ РѕРєРЅР° РЅР°СЃС‚СЂРѕРµРє
 	sf::Texture start_background;
 	start_background.loadFromFile("pic//start_back.png");
 	sf::Sprite start_background_sprite(start_background);
@@ -310,7 +310,7 @@ int main() {
 	
 	demonsration_setting_block(set_blocks_sprites, 5, set_block);
 
-	//главный цикл при открытом окне настроек************************************************************************
+	//РіР»Р°РІРЅС‹Р№ С†РёРєР» РїСЂРё РѕС‚РєСЂС‹С‚РѕРј РѕРєРЅРµ РЅР°СЃС‚СЂРѕРµРє************************************************************************
 	while (setting_window.isOpen()){
 		
 		setting_window.draw(start_background_sprite);
@@ -322,10 +322,10 @@ int main() {
 		
 		setting_window.draw(set_cage_sprite);
 		sf::Event event;
-		//отлавливаем слбытия
+		//РѕС‚Р»Р°РІР»РёРІР°РµРј СЃР»Р±С‹С‚РёСЏ
 		while (setting_window.pollEvent(event)) {
 
-			//если закрыли окно
+			//РµСЃР»Рё Р·Р°РєСЂС‹Р»Рё РѕРєРЅРѕ
 			if (event.type == sf::Event::Closed) {
 				background_sprite.setColor(set_color_sprite.getColor());
 				setting_window.close();
@@ -344,7 +344,7 @@ int main() {
 				if (event.key.code == (sf::Keyboard::W)) {
 				
 				}
-				//выбор
+				//РІС‹Р±РѕСЂ
 				if (event.key.code == sf::Keyboard::Enter) {
 					background_sprite.setColor(set_color_sprite.getColor());
 					setting_window.close();
@@ -360,23 +360,23 @@ int main() {
 		setting_window.display();
 	}
 
-	//главный цикл при открытом игровом окне**********************************************************************
+	//РіР»Р°РІРЅС‹Р№ С†РёРєР» РїСЂРё РѕС‚РєСЂС‹С‚РѕРј РёРіСЂРѕРІРѕРј РѕРєРЅРµ**********************************************************************
 	while (gaming_window.isOpen()) {
 
 		gaming_window.draw(background_sprite);
 
 		field[player.x][player.y].available = false;
 
-		//отлавливаем события
+		//РѕС‚Р»Р°РІР»РёРІР°РµРј СЃРѕР±С‹С‚РёСЏ
 		sf::Event event;
 		while (gaming_window.pollEvent(event)) {
 
-			//если закрыли окно
+			//РµСЃР»Рё Р·Р°РєСЂС‹Р»Рё РѕРєРЅРѕ
 			if (event.type == sf::Event::Closed) {
 				gaming_window.close();
 			}
 
-			//движение
+			//РґРІРёР¶РµРЅРёРµ
 			if (event.type == sf::Event::KeyPressed)
 			{
 				if (event.key.code == (sf::Keyboard::D)) {
@@ -385,35 +385,35 @@ int main() {
 				if (event.key.code == (sf::Keyboard::A)) {
 					left_was_pressed = true;
 				}
-				//ускоренное падение
+				//СѓСЃРєРѕСЂРµРЅРЅРѕРµ РїР°РґРµРЅРёРµ
 				if (event.key.code == (sf::Keyboard::S)) {
 					down_was_pressed = true;
 				}
 				
-				//переворот фигуры
+				//РїРµСЂРµРІРѕСЂРѕС‚ С„РёРіСѓСЂС‹
 				if (event.key.code == (sf::Keyboard::W)) {
 					
 				}
 			}
-			//проверяем, прошла ли наша задержка времени отклика
+			//РїСЂРѕРІРµСЂСЏРµРј, РїСЂРѕС€Р»Р° Р»Рё РЅР°С€Р° Р·Р°РґРµСЂР¶РєР° РІСЂРµРјРµРЅРё РѕС‚РєР»РёРєР°
 			if (pressing_timer.getElapsedTime() > pressing_delay) {
-				//нажатие клавиш
+				//РЅР°Р¶Р°С‚РёРµ РєР»Р°РІРёС€
 
-				//движение фигуры вправо
+				//РґРІРёР¶РµРЅРёРµ С„РёРіСѓСЂС‹ РІРїСЂР°РІРѕ
 				if (right_was_pressed) {
 					if (player.right_resolution()) {
 						player.x++;
 					}
 					right_was_pressed = false;
 				}
-				//двидение фигуры влево
+				//РґРІРёРґРµРЅРёРµ С„РёРіСѓСЂС‹ РІР»РµРІРѕ
 				if (left_was_pressed) {
 					if (player.left_resolution()) {
 						player.x--;
 					}
 					left_was_pressed = false;
 				}
-				//если была нажата кнопка для ускоренного падения
+				//РµСЃР»Рё Р±С‹Р»Р° РЅР°Р¶Р°С‚Р° РєРЅРѕРїРєР° РґР»СЏ СѓСЃРєРѕСЂРµРЅРЅРѕРіРѕ РїР°РґРµРЅРёСЏ
 				if (down_was_pressed) {
 					falling_delay = sf::seconds(0.03);
 					down_was_pressed = false;
@@ -432,16 +432,16 @@ int main() {
 
 		
 		
-		//падение 
+		//РїР°РґРµРЅРёРµ 
 		if (falling_timer.getElapsedTime() > falling_delay) {
 			player.fall();
 			falling_timer.restart();
 		}
 
-		//делаем активными блоки игрока
+		//РґРµР»Р°РµРј Р°РєС‚РёРІРЅС‹РјРё Р±Р»РѕРєРё РёРіСЂРѕРєР°
 		field[player.x][player.y].available = true;
 
-		//установка блока на поле, если больше не можем двигаться вниз
+		//СѓСЃС‚Р°РЅРѕРІРєР° Р±Р»РѕРєР° РЅР° РїРѕР»Рµ, РµСЃР»Рё Р±РѕР»СЊС€Рµ РЅРµ РјРѕР¶РµРј РґРІРёРіР°С‚СЊСЃСЏ РІРЅРёР·
 		if (!player.down_resolution()) {
 			if (check_line(player.y))
 			{
@@ -452,7 +452,7 @@ int main() {
 		}
 
 
-		//отрисовываем все активные блоки на поле
+		//РѕС‚СЂРёСЃРѕРІС‹РІР°РµРј РІСЃРµ Р°РєС‚РёРІРЅС‹Рµ Р±Р»РѕРєРё РЅР° РїРѕР»Рµ
 		for (int i = 0; i < FIELD_WIDTH; ++i) {
 			for (int j = 0; j < FIELD_HEIGHT; ++j) {
 				if (field[i][j].available) {
